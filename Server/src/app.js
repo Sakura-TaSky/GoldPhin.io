15c4ccc00a9157a080aa14e38967b22d95b12b0a
DoFlow-spec <majdeltacrypto@gmail.com>
2025-08-03 06:04:55 +0530

    cors

--- Server/src/app.js
+++ Server/src/app.js
@@ -10,29 +10,13 @@ dotenv.config({
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:3001",
-  "https://gold-phin-io.vercel.app",
-];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // ✅ Allow POST
+    origin: "*",
     credentials: true,
   })
 );
-
-app.options("*", cors());
-
 app.use(express.json());
 
 app.use("/swap", swapRoute);
