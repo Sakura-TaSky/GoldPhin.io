5a286aa06afabdd49f474f585dea6a658cde90bd
DoFlow-spec <majdeltacrypto@gmail.com>
2025-08-03 05:44:29 +0530

    cors test

--- Server/src/app.js
+++ Server/src/app.js
@@ -13,13 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3001",
-      "https://gold-phin-io.vercel.app",
-    ],
-    credentials: true,
+    origin: "*",
   })
 );
 app.use(express.json());
